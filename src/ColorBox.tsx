44afc6d3b5eb8a96c8571bc8c90dc7d764838367
Asif Mohammed <asifm2882@gmail.com>
2020-10-27 17:36:15 +0400

    add overlay message on copy to clipboard

--- src/ColorBox.tsx
+++ src/ColorBox.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Color } from './model/interface';
 import './ColorBox.css';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 const ColorBox: React.FC<Color> = ({ name, color }) => {
+  const [copied, setCopied] = useState(false);
+
+  const onCopyToClipBoard = (text: string, result: boolean) => {
+    setCopied(result);
+    setTimeout(() => setCopied(false), 1500);
+  };
   return (
-    <div style={{ background: color }} className="Colorbox">
-      <div className="Copy-container">
-        <div className="Box-content">
-          <span>{name}</span>
+    <CopyToClipboard text={color} onCopy={onCopyToClipBoard}>
+      <div style={{ background: color }} className="Colorbox">
+        <div
+          style={{ background: color }}
+          className={`Copy-overlay ${copied && 'show'}`}
+        />
+        <div className={`Copy-overlay-text ${copied && 'show'}`}>
+          <h1>copied!!</h1>
+          <p>{color}</p>
         </div>
-        <button className="Copy-button">copy</button>
+        <div className="Copy-container">
+          <div className="Box-content">
+            <span>{name}</span>
+          </div>
+          <button className="Copy-button">copy</button>
+        </div>
+        <div className="More-button">more</div>
       </div>
-      <div className="More-button">MORE</div>
-    </div>
+    </CopyToClipboard>
   );
 };
 
