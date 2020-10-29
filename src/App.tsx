b7d93c4bfab4fa2898b9fc417279444eb0c05c99
Asif Mohammed <asifm2882@gmail.com>
2020-10-29 14:44:35 +0400

    use generated palette

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import Palette from './Palette';
 import { seedColors } from './model/seedColors';
+import generatePalette from './utils/colorGenerator';
 
 function App() {
   return (
     // <div className="App">
-    <Palette {...seedColors[0]} />
+    <Palette {...generatePalette(seedColors[0])} />
     // </div>
   );
 }
