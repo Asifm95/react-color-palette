325ecb9f77b6217767ace8d461cdf89ca6d781be
Asif Mohammed <asifm2882@gmail.com>
2020-10-29 14:45:14 +0400

    update palette interface

--- src/model/interface.ts
+++ src/model/interface.ts
@@ -5,7 +5,16 @@ export interface PalleteType {
   colors: Color[];
 }
 
+export interface ColorPalette extends Omit<PalleteType, 'colors'> {
+  colors: Record<string, GeneratedColor[]>;
+}
+
 export interface Color {
   name: string;
   color: string;
+}
+export interface GeneratedColor extends Omit<Color, 'color'> {
+  id: string;
+  hex: string;
+  rgb: string;
 }
