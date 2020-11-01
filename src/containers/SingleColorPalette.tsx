fba2785bc5878554b9d482c16673e8823a07fa35
Asif Mohammed <asifm2882@gmail.com>
2020-11-01 17:55:02 +0400

    fix dropdown in singleColorPalette

--- src/containers/SingleColorPalette.tsx
+++ src/containers/SingleColorPalette.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import ColorBox from '../components/ColorBox';
 import NavBar from '../components/NavBar';
@@ -7,6 +7,8 @@ import { seedColors } from '../model/seedColors';
 import generatePalette, { findPalette } from '../utils/paletteHelper';
 import useStyles, { BackButton } from '../styles/SinglePaletteColorStyles';
 import PaletteFooter from '../components/PaletteFooter';
+import useSnackBar from '../hooks/useSnackBar';
+import SnackBar from '../components/SnackBar';
 
 type TParams = {
   paletteId: string;
@@ -27,13 +29,22 @@ const SingleColorPalette: React.FC<RouteComponentProps<TParams>> = ({
     seedColors,
     match.params.paletteId
   ) as PalleteType;
+
+  const [format, setFormat] = useState('hex');
+  const onFormatChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setFormat(event.target.value as string);
+    setTimeout(() => setOpen(true));
+  };
+
+  const [open, setOpen, handleClose] = useSnackBar(false);
+
   if (palette) {
     const genPalette = generatePalette(palette) as ColorPalette;
     const colorShades = generateShades(genPalette, match.params.colorId);
 
     return (
       <div className={classes.root}>
-        <NavBar format="hex" />
+        <NavBar format={format} formatChange={onFormatChange} />
         <div className={classes.paletteColors}>
           {colorShades &&
             colorShades.map((color, id) => (
@@ -41,7 +52,7 @@ const SingleColorPalette: React.FC<RouteComponentProps<TParams>> = ({
                 key={id}
                 singleMode={true}
                 paletteId={genPalette?.id as string}
-                format={'hex'}
+                format={format}
                 color={color}
               />
             ))}
@@ -56,6 +67,7 @@ const SingleColorPalette: React.FC<RouteComponentProps<TParams>> = ({
           </div>
         </div>
         <PaletteFooter name={genPalette.paletteName} emoji={genPalette.emoji} />
+        <SnackBar message="Format changed" open={open} onClose={handleClose} />
       </div>
     );
   } else {
