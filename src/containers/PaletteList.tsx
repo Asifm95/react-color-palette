d7a3c57e387c8847354b576dc617c95dbb8f6552
Asif Mohammed <asifm2882@gmail.com>
2020-11-01 16:42:15 +0400

    add new palette button

--- src/containers/PaletteList.tsx
+++ src/containers/PaletteList.tsx
@@ -1,15 +1,17 @@
+import Button from '@material-ui/core/Button';
 import React from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import MiniPalette from '../components/MiniPalette';
 import { seedColors } from '../model/seedColors';
 import useStyles from '../styles/PaletteListStyles';
+import AddIcon from '@material-ui/icons/Add';
 
 const PaletteList: React.FC<RouteComponentProps<{}>> = ({ history }) => {
   const classes = useStyles();
   const paletteClickHandler = (id: string) => history.push(`/palette/${id}`);
 
   const list = seedColors.map((color) => (
-    <MiniPalette paletteClick={paletteClickHandler} {...color} />
+    <MiniPalette key={color.id} paletteClick={paletteClickHandler} {...color} />
   ));
 
   return (
@@ -17,7 +19,11 @@ const PaletteList: React.FC<RouteComponentProps<{}>> = ({ history }) => {
       <div className={classes.container}>
         <nav className={classes.nav}>
           <h1 className={classes.heading}>React Colors</h1>
-          <Link to="/palette/new">Create Palette</Link>
+          {/* <Link to="/palette/new"> */}
+          <Button variant="outlined" color="default" startIcon={<AddIcon />}>
+            new palette
+          </Button>
+          {/* </Link> */}
         </nav>
         <div className={classes.palette}>{list}</div>
       </div>
