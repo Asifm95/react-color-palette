532001319ec67e495a4786e3d2d97b711d2f91ff
Asif Mohammed <asifm2882@gmail.com>
2020-11-01 16:41:16 +0400

    add single palette route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import './App.css';
 import Palette from './containers/Palette';
 import { Switch, Route } from 'react-router-dom';
 import PaletteList from './containers/PaletteList';
+import SingleColorPalette from './containers/SingleColorPalette';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={PaletteList} />
       <Route exact path="/palette/:id" component={Palette} />
+      <Route
+        exact
+        path="/palette/:paletteId/:colorId"
+        component={SingleColorPalette}
+      />
     </Switch>
   );
 }
